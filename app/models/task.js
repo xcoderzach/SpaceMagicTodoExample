aa2f36ea0efb81bf502811f33fed66e1271c3015
Zach Smith <x.coder.zach@gmail.com>
2012-04-13 03:07:32 -0500

    updated tasks to cull

--- app/models/task.js
+++ app/models/task.js
@@ -1,6 +1,17 @@
 var Document = require("LiveDocument/lib/document")
 
-module.exports = Document.define("Task")
+var Task = module.exports = Document.define("Task")
   .key("title", {min: 1})
   .key("votes")
   .key("done") 
+  .afterSave(function() {
+    //cull some chats, to keep the number down
+    Task.find({}, function(tasks) {
+      var num = tasks.length - 50
+      tasks.sortBy("createdAt", true)
+      console.log(num)
+      for(i = 0 ; i < num ; i++) {
+        tasks.at(tasks.length - (i+1)).remove()
+      }
+    })
+  }) 
